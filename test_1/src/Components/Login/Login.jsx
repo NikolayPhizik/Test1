791593daa9d16f808dccb15c2d12b36f3bf25854
Nikolay <nikolay.eremin82@mail.ru>
2021-11-07 15:32:06 +0600

    Сделал визуальную часть формы авторизации

--- test_1/src/Components/Login/Login.jsx
+++ test_1/src/Components/Login/Login.jsx
@@ -1,6 +1,24 @@
+import classes from "./Login.module.css";
+import classNames from "classnames";
+
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <section className={classes.modal}>
+            <div className={classes.modal_login}>
+                <h2 className={classes.modal_login_h2}>Личный кабинет</h2>
+                <p>Введите свой логин и пароль.</p>
+                <form>
+                    <p className={classes.login_form_p}>
+                        <input className={classNames(classes.login_form_input, classes.login_icon_user)} type="text" name="login" placeholder="Логин"/>
+                    </p>
+                    <p className={classes.login_form_p}>
+                        <input className={classNames(classes.login_form_input, classes.login_icon_password)} type="password" name="password"
+                               placeholder="Пароль"/>
+                    </p>
+                    <button className={classes.button} type="submit">Войти</button>
+                </form>
+            </div>
+        </section>
     );
 };
 
