834fb8289aa651ed00ec10064e27eae6b4393398
Nikolay <nikolay.eremin82@mail.ru>
2021-11-21 16:57:16 +0600

    Подключил Redux-form для логина, а также подправил визуальную часть

--- test_1/src/Components/Login/Login.jsx
+++ test_1/src/Components/Login/Login.jsx
@@ -1,25 +1,46 @@
 import classes from "./Login.module.css";
 import classNames from "classnames";
+import {Field, reduxForm} from "redux-form";
 
 const Login = () => {
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    };
+
     return (
         <section className={classes.modal}>
             <div className={classes.modal_login}>
-                <h2 className={classes.modal_login_h2}>Личный кабинет</h2>
-                <p>Введите свой логин и пароль.</p>
-                <form>
-                    <p className={classes.login_form_p}>
-                        <input className={classNames(classes.login_form_input, classes.login_icon_user)} type="text" name="login" placeholder="Логин"/>
-                    </p>
-                    <p className={classes.login_form_p}>
-                        <input className={classNames(classes.login_form_input, classes.login_icon_password)} type="password" name="password"
-                               placeholder="Пароль"/>
-                    </p>
-                    <button className={classes.button} type="submit">Войти</button>
-                </form>
+                <h2 className={classes.modal_login_h2}>Personal area</h2>
+                <p>Enter your login and password</p>
+                <LoginReduxForm onSubmit={onSubmit} />
             </div>
         </section>
     );
 };
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <p className={classes.login_form_p}>
+                <Field className={classNames(classes.login_form_input, classes.login_icon_user)}
+                       type="text"
+                       name="login"
+                       placeholder="Логин"
+                       component="input" />
+            </p>
+            <p className={classes.login_form_p}>
+                <Field className={classNames(classes.login_form_input, classes.login_icon_password)}
+                       type="password"
+                       name="password"
+                       placeholder="Пароль"
+                       component="input" />
+            </p>
+            <button className={classes.button} type="submit">Login</button>
+        </form>
+    );
+};
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 export default Login;
