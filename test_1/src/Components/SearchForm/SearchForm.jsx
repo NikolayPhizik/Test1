c433c2c5cb13de70ccc0589313895c3a9aa93c56
Nikolay <nikolay.eremin82@mail.ru>
2021-11-21 16:13:44 +0600

    Подключил Redux-form и доделал поиск по контактам

--- test_1/src/Components/SearchForm/SearchForm.jsx
+++ test_1/src/Components/SearchForm/SearchForm.jsx
@@ -1,20 +1,36 @@
 import classes from "./SearchForm.module.css";
+import {Field, reduxForm} from "redux-form";
 
 
 const SearchForm = ({searchUser}) => {
+
+    const onSubmit = (formData) => {
+        searchUser(formData.search);
+    }
 
     return (
         <section>
             <div className={classes.main_wrapper}>
                 <div className={classes.form_container}>
-                    <form className={classes.form}>
-                        <input className={classes.input} type="text" name="search" placeholder="Enter a name to search" />
-                        <span onClick={(e) => {searchUser("RONNIE BALISTRERI")}} className={classes.span}>Search</span>
-                    </form>
+                    <SearchReduxForm onSubmit={onSubmit}/>
                 </div>
             </div>
         </section>
     );
 };
+
+const Form = (props) => {
+    return (
+        <form className={classes.form} onSubmit={props.handleSubmit}>
+            <Field className={classes.input}
+                   name="search"
+                   placeholder="Enter a name to search"
+                   component="input" type="text" />
+            <button className={classes.button} type="submit">Search</button>
+        </form>
+    );
+};
+
+const SearchReduxForm = reduxForm({form: 'search'})(Form);
 
 export default SearchForm;
--- test_1/src/Redux/Store.js
+++ test_1/src/Redux/Store.js
@@ -1,10 +1,12 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {usersReducer} from "./users-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 
 let reducers = combineReducers({
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
