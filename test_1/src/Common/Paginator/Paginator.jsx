8f6feffd418ba1661b7237279a2b175fc50d1250
Nikolay <nikolay.eremin82@mail.ru>
2021-12-14 11:39:32 +0600

    Доработал пагинатор

--- test_1/src/Common/Paginator/Paginator.jsx
+++ test_1/src/Common/Paginator/Paginator.jsx
@@ -5,24 +5,34 @@ import {useState} from "react";
 
 const Paginator = ({pageSize, totalUsersCount, requestTotalUsersCount, onPageChanged}) => {
 
+    const [portionNumber, setpPortionNumber] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+
     let pagesCount = Math.ceil(totalUsersCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
-    const [currentPage, setCurrentPage] = useState(1);
+    let portionSize = 3;
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
 
     requestTotalUsersCount();
     return (
-        <span className={classes.paginator_box}>
-            {pages.map(p => {
-                return <div className={classNames({[classes.selectedPage]: currentPage === p}, classes.container)} key={p} onClick={(e) => {
+        <div className={classes.paginator_box}>
+            {portionNumber > 1 && <button onClick={() => {setpPortionNumber(portionNumber - 1)}}>Back</button>}
+            {pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber).map(p => {
+                return <div className={classNames(
+                    {[classes.selectedPage]: currentPage === p}, 
+                    classes.container)} key={p} onClick={(e) => {
                     onPageChanged(p);
                     setCurrentPage(p);
                 }}>{p}</div>
             })}
-        </span>
+            {portionCount > portionNumber && <button onClick={() => {setpPortionNumber(portionNumber + 1)}}>Next</button>}
+        </div>
     );
 };
 
